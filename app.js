d7e3e06e9ded5c5ebcf0ed9f73a0cd62453d5a1e
Klevis Xhyra <klevissxh@gmail.com>
2024-10-18 22:21:54 +0200

    change page title

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Leben in DEUTSCHLAND 🇩🇪</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
